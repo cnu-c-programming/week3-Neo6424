4fb89b9ec195b190471bfc005df6b6c223022657
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-03-20 10:41:29 +0900

    Update 3-5.c

---
+++ code/3-5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int my_sum(char c, int num, ...) {
+  va_list args;
+  va_start(args, num);
+  
+  switch(c) {
+    case 'S':
+      for(int i =0; i <num; i++){
+        printf("%s",va_arg(args, char*));
+      }
+      printf("\n");
+      break;
+    case 'C':
+      for(int i =0; i <num; i++){
+        printf("%c",va_arg(args, int));
+      }
+      printf("\n");
+      break;
+    case 'D':
+      int sum =0;
+      for(int i =0; i <num; i++){
+        sum = sum + va_arg(args, int);
+      }
+      printf("%d\n",sum);
+      break;
+  }
+  va_end(args);
+}
+
+int main() {
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
+
+    return 0;
+}
